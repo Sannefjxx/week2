5badfd7356cd795f26ad8fdaaa77f9cae896c1ef
Sanne Crawford <s1184451@student.windesheim.nl>
2022-09-13 15:45:37 +0200

    v1.3

--- 3.19/3.19/319.cpp
+++ 3.19/3.19/319.cpp
@@ -6,21 +6,37 @@ int main(void) {
 	int principal;
 	int rate;
 	int day;
-
-
-	printf("Enter loan principal (-1 to end):");
-	scanf_s("%d", &principal);
-
-	if (principal > 0) {
-		
+	int teller;
+	teller = 0;
+	if (teller = 0) {
+		teller = teller + 1;
+		printf("Enter loan principal:");
+		scanf_s("%f", &principal);
+	}
+	while (teller == 1) {
+		teller = teller + 1;
 		printf("Enter interest rate:");
-		scanf_s("%d", &rate);
+		scanf_s("%f", &rate);
 		printf("Enter term of the loan in days:");
-		scanf_s("%d", &day);
-		interest = (float) principal * rate * (day / 365);
+		scanf_s("%f", &day);
+		interest = principal * rate * (day / 365);
 		printf("The interest charge is $ %f\n", interest);
 	}
-	else {
+	while(teller > 1){
+		teller = teller + 1;
+
+		printf("Enter loan principal (-1 to end):");
+		scanf_s("%f", &principal);
+		printf("Enter interest rate:");
+		scanf_s("%f", &rate);
+		printf("Enter term of the loan in days:");
+		scanf_s("%f", &day);
+		interest = principal * rate * (day / 365);
+		printf("The interest charge is $ %f\n", interest);
+		
+	}
+	if (principal != -1) {
 		printf("END");
+
 	}
 }
