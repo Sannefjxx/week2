60a16acc6de0f67c85b9fa503f7053f898a29380
Sanne Crawford <s1184451@student.windesheim.nl>
2022-09-14 10:49:30 +0200

    v5.1

--- 3.19/3.19/3.23.cpp
+++ 3.19/3.19/3.23.cpp
@@ -2,11 +2,25 @@
 
 int main(void){
 
-	int counter;
 	int number;
 	int largest;
 
+	largest = 1;
 
+	for (int counter = 1; counter <= 10; counter++) {
+
+		if (counter <= 10) {
+
+			printf("Enter a number: ");
+			scanf_s("%d", &number);
+		}
+		if(number >= largest) {
+			largest = number;
+			
+		}
+		
+	}
+	printf("largest is: %d", largest);
 
 
 	}
